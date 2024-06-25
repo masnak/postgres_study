5b25e15a78f990ffc367e4c918903b9002a220b4
Masamichi Nakamura <masamichi.nakamura@gmail.com>
2024-06-25 20:01:40 +0900

    create login.sh

--- login.sh
+++ login.sh
@@ -6,5 +6,11 @@ PERSISTENT_VOLUME_NAME="postgres_db"
 # コンテナ名を変数に格納
 CONTAINER_NAME="postgres-con"
 
-# コンテナにログイン
-docker exec -it $CONTAINER_NAME bash
+# コンテナが実行中かどうか確認
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    # コンテナにログイン
+    docker exec -it $CONTAINER_NAME bash
+else
+    echo "エラー: コンテナ '$CONTAINER_NAME' は実行中ではありません。"
+    exit 1
+fi
